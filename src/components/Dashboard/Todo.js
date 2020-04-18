3042ea209f7cf91840c201ed755c70ee30e4f6cd
Fiifi1 <freshprince708@yahoo.com>
2020-04-18 18:40:51 +0000

    Todo frontend ready for check

--- src/components/Dashboard/Todo.js
+++ src/components/Dashboard/Todo.js
@@ -1,125 +1,93 @@
-import React, { setState } from 'react';
-
-let state = {
-  todos: [],
-  todoToShow: 'all',
-  toggleAllComplete: true,
-};
-
-let addTodo = (todo) => {
-  this.setState((state) => ({
-    todos: [todo, ...state.todos],
-  }));
-};
-
-let toggleComplete = (id) => {
-  setState((state) => ({
-    todos: state.todos.map((todo) => {
-      if (todo.id === id) {
-        // suppose to update
-        return {
-          ...todo,
-          complete: !todo.complete,
-        };
-      } else {
-        return todo;
-      }
-    }),
-  }));
-};
-
-let updateTodoToShow = (s) => {
-  setState({
-    todoToShow: s,
-  });
-};
-
-let handleDeleteTodo = (id) => {
-  setState((state) => ({
-    todos: state.todos.filter((todo) => todo.id !== id),
-  }));
-};
-
-let removeAllTodosThatAreComplete = () => {
-  setState((state) => ({
-    todos: state.todos.filter((todo) => !todo.complete),
-  }));
-};
-
-let todos = [];
-
-if (state.todoToShow === 'all') {
-  todos = state.todos;
-} else if (state.todoToShow === 'active') {
-  todos = state.todos.filter((todo) => !todo.complete);
-} else if (state.todoToShow === 'complete') {
-  todos = state.todos.filter((todo) => todo.complete);
-}
+import React from 'react';
 
 const Todo = () => {
-  return (
-    <div>
-      <div onSubmit={addTodo} />
-      {todos.map((todo) => (
-        <div
-          key={todo.id}
-          toggleComplete={() => toggleComplete(todo.id)}
-          onDelete={() => handleDeleteTodo(todo.id)}
-          todo={todo}
-        />
-      ))}
-      <div>
-        todos left: {state.todos.filter((todo) => !todo.complete).length}
-      </div>
-      <div>
-        <button onClick={() => updateTodoToShow('all')}>all</button>
-        <button onClick={() => updateTodoToShow('active')}>active</button>
-        <button onClick={() => updateTodoToShow('complete')}>complete</button>
-      </div>
-      {state.todos.some((todo) => todo.complete) ? (
-        <div>
-          <button onClick={removeAllTodosThatAreComplete}>
-            remove all complete todos
-          </button>
+  
+    return (
+     <div className="row col-md-12">
+        <div className="col-md-4">
+            <div className="card-header py-3">
+                <h6 className="text-primary font-weight-bold m-0">Add A New Item</h6>
+            </div>
+            <div className="form">
+                <div className="form-group">
+                <   input type="email" className="form-control" placeholder="Add Item To List" id="todoItem"/>
+                </div>
+                <div className="form-group">
+                    <label for="taskDate">Due date:</label>
+                    <input type="date" className="form-control" id="taskDate"/>
+                    <label for="taskTime">Time:</label>
+                    <input type="time" className="form-control" id="taskTime"/>
+                </div>
+                <div className="col p-2">
+                    <button
+                      type="button"
+                      className="btn btn-outline-primary btn-sm btn-block"
+                    >
+                      Add
+                    </button>
+                  </div>
+            </div>
         </div>
-      ) : null}
-      <div>
-        <button
-          onClick={() =>
-            this.setState((state) => ({
-              todos: state.todos.map((todo) => ({
-                ...todo,
-                complete: state.toggleAllComplete,
-              })),
-              toggleAllComplete: !state.toggleAllComplete,
-            }))
-          }
-        >
-          toggle all complete: {`${state.toggleAllComplete}`}
-        </button>
-      </div>
-    </div>
+        <div className="col-md-8">
+            <div className="card-header py-3">
+                <h6 className="text-primary font-weight-bold m-0">To do List</h6>
+            </div>
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item">
+                    <div className="row align-items-center no-gutters">
+                        <div className="col mr-2">
+                            <p className="mb-0">
+                                <strong>General Meeting With Riders</strong>
+                            </p>
+                            <span className="text-xs mr-5">10:30 AM</span>
+                            <span className="text-xs ml-5" id="date">08/05/2020</span>
+                        </div>
+                         <div className="col-auto">
+                            <div className="custom-control custom-checkbox">
+                                <input className="custom-control-input" type="checkbox" id="task-1"/>
+                                <label className="custom-control-label" for="task-1"></label>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li className="list-group-item">
+                    <div className="row align-items-center no-gutters">
+                        <div className="col mr-2">
+                            <p className="mb-0">
+                                <strong>Call a customer with id # 0101012</strong>
+                            </p>
+                            <span className="text-xs mr-5" id="time">11:30 AM</span>
+                            <span className="text-xs ml-5" id="date">18/04/2020</span>
+                        </div>
+                        <div className="col-auto">
+                            <div className="custom-control custom-checkbox">
+                                <input className="custom-control-input" type="checkbox" id="task-2"/>
+                                <label className="custom-control-label" for="task-2"></label>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li className="list-group-item">
+                    <div className="row align-items-center no-gutters">
+                        <div className="col mr-2">
+                            <p className="mb-0">
+                                <strong>Meet a new client for a contract</strong>
+                            </p>
+                            <span className="text-xs mr-5">2:30 PM</span>
+                            <span className="text-xs ml-5" id="date">15/04/2020</span>
+                        </div>
+                         <div className="col-auto">
+                            <div className="custom-control custom-checkbox">
+                                <input className="custom-control-input" type="checkbox" id="task-3"/>
+                                <label className="custom-control-label" for="task-3"></label>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        </div>
   );
 };
 
 export default Todo;
-
-/*
-<div className ="col-lg-6 mb-4">
-<div className="card shadow mb-4">
-  <div className="card-header py-3">
-      <h6 className="text-primary font-weight-bold m-0">Todo List</h6>
-  </div>
-  <ul className="list-group list-group-flush">
-      <li className="list-group-item">
-          <div className="row align-items-center no-gutters">
-              <div className="col mr-2">
-                  <h6 className="mb-0"><strong>Lunch meeting</strong></h6><span className="text-xs">10:30 AM</span></div>
-              <div className="col-auto">
-                  <div className="custom-control custom-checkbox"><input className="custom-control-input" type="checkbox" id="formCheck-1"><label className="custom-control-label" for="formCheck-1"></label></div>
-              </div>
-          </div>
-      </li>
-  </ul>
-</div>
-</div>*/
